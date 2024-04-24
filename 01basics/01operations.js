caef5ef57ddf19ecae0cda1a89bad0a3b63668fb
manasashebbar <161590161+manasashebbar@users.noreply.github.com>
2024-04-24 05:54:17 +0000

    operations

--- 01basics/01operations.js
+++ 01basics/01operations.js
@@ -2,3 +2,13 @@ let value = 4;
 let negValue = -value;
 
 console.log(negValue);
+
+let str1 = "MANASA";
+let str2 = " Hebbar";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log(1+2 + "2");
+
